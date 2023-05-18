0e337117d665b0ffc1d2de2d819837a3af0bd069
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-05-18 19:58:29 +0300

    next projects

---
+++ 06. Repainting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06._Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nylon = int.Parse(Console.ReadLine())+2;
+            double paint = int.Parse(Console.ReadLine());
+            paint = paint + paint * 0.1;   
+            int thinner = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            double nylonPrice = nylon * 1.5;
+            double paintPrice = paint * 14.5;
+            double thinnerPrice = thinner * 5;
+            double sum = nylonPrice + paintPrice + thinnerPrice + 0.4;
+            double priceM = sum * 0.3;
+            double allPrice = sum + priceM * hours;
+            Console.WriteLine(allPrice);
+            
+        }
+    }
+}
---
+++ 07. Food Delivery/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07._Food_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chicken = int.Parse(Console.ReadLine());
+            int fish = int.Parse(Console.ReadLine());
+            int vegan = int.Parse(Console.ReadLine());
+            double chickenPrice = chicken * 10.35;
+            double fishPrice = fish * 12.4;
+            double veganPrice = vegan * 8.15;
+            double sum = chickenPrice + fishPrice + veganPrice;
+            double dessert = sum * 0.2;
+            double delivery = 2.5;
+            double all = sum + dessert + delivery;
+            Console.WriteLine(all);
+        }
+    }
+}
