ea94b98c84cbf199b606849145d7b7f7ac6b6d85
Ивайло Шопов <is41062491@edu.mon.bg>
2023-05-18 19:29:05 +0300

    Add project files.

---
+++ 03. Deposit Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03._Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            double rate = double.Parse(Console.ReadLine())/100;
+            double sum = deposit + month * ((deposit * rate) / 12);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ Fish Tank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double rate = double.Parse(Console.ReadLine())/100;
+            double capacity = length * width * height;
+            double capacityinLitters = capacity / 1000;
+            double necessaryLitters = capacityinLitters * (1 - rate);
+            Console.WriteLine(necessaryLitters);
+        }
+    }
+}
